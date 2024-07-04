2bd51e2fb37d18c472d955ad268cd60f169e5f7b
RaccHoon <choco730522@gmail.com>
2024-07-04 20:20:47 +0900

    init

---
+++ src/main/java/cnsa/demo/controller/homeController.java
@@ -0,0 +1,19 @@
+package cnsa.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("welcomeMessage", "This is Homepage");
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
